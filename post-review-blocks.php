f1b909297cc531d9db08f7aedcc3f45747d5bc17
Nate Finch <nfinch2@wisc.edu>
2024-05-10 12:03:45 -0500

    remove comment

--- post-review-blocks.php
+++ post-review-blocks.php
@@ -17,13 +17,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-/**
- * Registers the block using the metadata loaded from the `block.json` file.
- * Behind the scenes, it registers also all assets so they can be enqueued
- * through the block editor in the corresponding context.
- *
- * @see https://developer.wordpress.org/reference/functions/register_block_type/
- */
 function create_block_post_review_blocks_block_init() {
 	register_block_type( __DIR__ . '/build/rating-block' );
 	register_block_type( __DIR__ . '/build/review-card-block' );
